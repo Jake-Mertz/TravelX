909087089d5e38e07c48cd30d14aed1583cc152d
Jake-Mertz <jakemertz101@gmail.com>
2021-03-05 15:17:53 -0500

    working on photo back end endpoint

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const db = require('./database');
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
+const uploadsMiddleware = require('./uploads-middleware');
 
 const app = express();
 
@@ -22,7 +23,38 @@ app.get('/api/health-check', (req, res, next) => {
 // new stuff goes here
 
 // Client uploads profile photo
-app.post('api/uploads');
+app.post('api/uploads', uploadsMiddleware, (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    throw new ClientError(400, 'name is a required field');
+  }
+
+  const image = `/userTable/${req.file.filename}`;
+  const imageSQL = `
+    insert into "userTable" ("profilePhotoUrl", "name")
+    values ($1, $2)
+    returning "profilePhotoUrl", "name"
+  `;
+  const imageParams = [image, name];
+  db.query(imageSQL, imageParams)
+    .then(result => {
+      const storedImage = result.rows;
+      res.status(200).json(storedImage);
+    })
+    .catch(err => next(err));
+});
+
+// app.get('/api/userTable', (req, res, next) => {
+//   const sql = `
+//     select "profilePhotoUrl"
+//       from "userTable"
+//   `;
+//   db.query(sql)
+//     .then(result => {
+//       res.json(result.rows);
+//     })
+//     .catch(err => next(err));
+// });
 
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
