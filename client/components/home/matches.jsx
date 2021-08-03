0fe5ef5b00294d3a6f0ee76183d2d1ecf77b666f
Jake-Mertz <jakemertz101@gmail.com>
2021-08-03 13:39:54 -0400

    styled components for matches component

--- client/components/home/matches.jsx
+++ client/components/home/matches.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+
+const Container = styled.div`
+  width: 65%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Heading = styled.h2`
+  font-size: ${rem(32)};
+  font-style: bold;
+  padding-top: 5%;
+  width: 20%;
+`;
+
+const SubHeading = styled.h3`
+  font-size: ${rem(18)};
+  width: 20%;
+`;
+
+const MatchesWrap = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: left;
+`;
+
+const MatchesList = styled.div`
+  width: 80%;
+  display: flex;
+`;
 
 function Matches(props) {
   return (
-    <div className="matches-container-container-container">
-      <div className="matches-container-container">
-        <h1 className="matches-list-title">Your Matches:</h1>
-        <h2 className="matches-subhead">Start a conversation</h2>
-        <div className="matches-container">
-          <div className="matches-list">{props.renderMatches}</div>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Section>
+        <Heading>Your Matches:</Heading>
+        <SubHeading>Start a conversation</SubHeading>
+        <MatchesWrap>
+          <MatchesList>{props.renderMatches}</MatchesList>
+        </MatchesWrap>
+      </Section>
+    </Container>
   );
 }
 
