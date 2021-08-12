483040cef027bb6dec9cd185a9551f93be7f1ba7
Jake-Mertz <jakemertz101@gmail.com>
2021-08-12 19:34:00 -0400

    just the beginning...

---
+++ client/components/app2.jsx
@@ -0,0 +1,26 @@
+// import React, { useState } from 'react';
+// import LandingPage from './landing/landing-page';
+// import SignUpPhoto from './landing/sign-up-photo';
+// import SignUpInterests from './landing/sign-up-interests';
+// import Login from './landing/login-page';
+// import SignUpCredentials from './landing/sign-up-creds';
+// import HomePage from './home/home-page';
+// import ProfilePage from './profile-page';
+
+// const App = props => {
+//   const [page, setPage] = useState(props.page);
+
+//   renderPage() {
+//     switch(page) {
+//       case 'landing-page':
+//         setPage('landing-page');
+//         break;
+//       default:
+//         setPage('landing-page');
+//     }
+//   }
+
+//   return (
+//     <div>{appView}</div>
+//   );
+// };
